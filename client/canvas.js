9f7b5beb8b820e7bec11dfee961ef9e529d77747
Ming Fong <mingfong07@hotmail.com>
2020-06-16 00:01:37 -0700

    initial commit

---
+++ client/canvas.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = 800;
+    canvas.width = 800;
+
+    let painting = false;
+
+    function startPosition() {
+        painting = true;
+    }
+    function endPosition() {
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e) {
+        if (!painting) {
+            return
+        }
+        ctx.lineWidth = 8;
+        ctx.lineCap = "round";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", endPosition);
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseout", endPosition)
+});
