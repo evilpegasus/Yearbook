ba6ad1fcedd1957f34d4b9c5622ee536ed03605e
Ming Fong <mingfong07@hotmail.com>
2020-06-22 00:17:29 -0700

    upload

--- public/upload.js
+++ public/upload.js
@@ -7,44 +7,44 @@ function upload() {
     canvas.toBlob(function(blob){
         var image = new Image();
         image.src = blob;
-        var uploadTask = storageRef.child(test).put(blob);
+        var uploadTask = storageRef.child('test').put(blob);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+        }, function(error) {
+
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+        case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+
+        case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+        case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+        }, function() {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        console.log('File available at', downloadURL);
+        });
+        });
     });
-
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function(snapshot) {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-        break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-        break;
-    }
-    }, function(error) {
-
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-    case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-
-    case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-    case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
-    }, function() {
-    // Upload completed successfully, now we can get the download URL
-    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-    console.log('File available at', downloadURL);
-    });
-});
 }
