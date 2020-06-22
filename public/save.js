058cbcafdb9b1f0d9b7c46182941c63043c84391
Ming Fong <mingfong07@hotmail.com>
2020-06-22 00:53:34 -0700

    draw image from server

--- public/save.js
+++ public/save.js
@@ -48,4 +48,44 @@ function upload() {
         });
         });
     });
+};
+function getImage() {
+    const canvas = document.querySelector("#canvas");
+
+    // Create a reference with an initial file path and name
+    var storage = firebase.storage();
+    var pathReference = storage.ref('test.png');
+
+    // Get the download URL
+    pathReference.getDownloadURL().then(function(url) {
+
+        // Draw the png file onto the canvas
+        drawing = new Image();
+        drawing.src = url; // can also be a remote URL e.g. http://
+        drawing.onload = function() {
+        context.drawImage(drawing,0,0);
+        };
+        console.log("Image from server drawn onto canvas. URL = ", url);
+    }).catch(function(error) {
+    
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+        case 'storage/object-not-found':
+            // File doesn't exist
+            break;
+    
+        case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+    
+        case 'storage/canceled':
+            // User canceled the upload
+            break;
+    
+        case 'storage/unknown':
+            // Unknown error occurred, inspect the server response
+            break;
+        }
+    });
 }
