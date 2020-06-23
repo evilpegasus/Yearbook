9b694296196c1458cddd583de60f7bf36ca8e495
Ming Fong <mingfong07@hotmail.com>
2020-06-22 21:37:24 -0700

    landing page as index.html

--- public/auth.js
+++ public/auth.js
@@ -26,7 +26,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'https://yearbook-hhs.web.app/defualt.html',
+    signInSuccessUrl: 'https://yearbook-hhs.web.app/app.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
