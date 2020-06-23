72cb8128d6e673d348faae67d954540afbe156fc
arnavthareja <arnav.thareja25@gmail.com>
2020-06-22 18:59:22 -0700

    Clean up code

--- public/canvas.js
+++ public/canvas.js
@@ -5,18 +5,16 @@ window.addEventListener('load', () => {
 
     // create the buttons on the canvas
     const clearButton = document.querySelector("#clearButton");
+    const downloadButton = document.querySelector("#downloadButton");
     const colorSelector = document.querySelector("#colorSelector");
     const widthSelector = document.querySelector("#widthSelector");
 
     // set canvas properties
     canvas.height = 500;
     canvas.width = 500;
-
     var painting = false;
     var offsetY = canvas.getBoundingClientRect().top;
     var offsetX = canvas.getBoundingClientRect().left;
-    // var color = "black";
-    // var size = 8;
 
     // set a white background
     ctx.fillStyle = "white";
@@ -65,46 +63,8 @@ window.addEventListener('load', () => {
         }
     }
 
-    /*
-    // change the drawing color
-    function changeColor(newColor) {
-        color = newColor;
-        if (color === "White") {
-            document.getElementById("activeColor").innerHTML = "Active tool: Eraser";
-        } else {
-            document.getElementById("activeColor").innerHTML = "Active color: " + newColor;
-        }
-    }
-
-    blackButton.onclick = function() {
-        changeColor("Black");
-    }
-    blueButton.onclick = function() {
-        changeColor("Blue");
-    }
-    redButton.onclick = function() {
-        changeColor("Red");
-    }
-    greenButton.onclick = function() {
-        changeColor("Green");
-    }
-
-    // eraser tool
-    const eraser = document.getElementById("eraser");
-    eraser.onclick = function() {
-        changeColor("White");
-    }
-
-    // size slider
-    const sizeSlider = document.getElementById("sizeSlider");
-    sizeSlider.oninput = function() {
-        size = this.value;
-        document.getElementById("thickness").innerHTML = "Thickness: " + size;
-    }
-    */
-
     // download the image
-    function download() {
+    downloadButton.onclick = function() {
         var download = document.querySelector("#download");
         var image = document.querySelector("#canvas").toDataURL("image/png")
                 .replace("image/png", "image/octet-stream");
--- public/save.js
+++ public/save.js
@@ -10,44 +10,45 @@ function upload() {
         // Listen for state changes, errors, and completion of the upload.
         uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
         function(snapshot) {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-        }
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
         }, function(error) {
             console.log("Failed to upload image to the server.");
             console.log(error);
             // A full list of error codes is available at
             // https://firebase.google.com/docs/storage/web/handle-errors
             switch (error.code) {
-            case 'storage/unauthorized':
-                // User doesn't have permission to access the object
-                break;
+                case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
 
-            case 'storage/canceled':
-                // User canceled the upload
-                break;
+                case 'storage/canceled':
+                    // User canceled the upload
+                    break;
 
-            case 'storage/unknown':
-                // Unknown error occurred, inspect error.serverResponse
-                break;
-        }
+                case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    break;
+            }
         }, function() {
         // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-        console.log('File available at', downloadURL);
-        window.alert("Upload successful");
-        });
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log('File available at', downloadURL);
+                window.alert("Upload successful");
+            });
         });
     });
 };
+
 function getImage() {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
@@ -61,11 +62,9 @@ function getImage() {
 
         // Draw the png file onto the canvas
         drawing = new Image();
-        // drawing.setAttribute('crossOrigin', 'use-credentials');
         drawing.src = url; // can also be a remote URL e.g. http://
-        // var timestamp = new Date().getTime();
         drawing.onload = function() {
-        ctx.drawImage(drawing,0,0);
+            ctx.drawImage(drawing,0,0);
         };
         console.log("Image from server drawn onto canvas. URL = ", url);
     }).catch(function(error) {
@@ -75,21 +74,21 @@ function getImage() {
         // A full list of error codes is available at
         // https://firebase.google.com/docs/storage/web/handle-errors
         switch (error.code) {
-        case 'storage/object-not-found':
-            // File doesn't exist
-            break;
-    
-        case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-    
-        case 'storage/canceled':
-            // User canceled the upload
-            break;
-    
-        case 'storage/unknown':
-            // Unknown error occurred, inspect the server response
-            break;
+            case 'storage/object-not-found':
+                // File doesn't exist
+                break;
+        
+            case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+        
+            case 'storage/canceled':
+                // User canceled the upload
+                break;
+        
+            case 'storage/unknown':
+                // Unknown error occurred, inspect the server response
+                break;
         }
     });
 }
