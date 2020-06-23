28ae813f998040a2eb14c878ba4980b4dc6f2d42
Ming Fong <mingfong07@hotmail.com>
2020-06-22 18:23:22 -0700

    sign in ui

--- public/auth.js
+++ public/auth.js
@@ -11,33 +11,60 @@ ui.start('#firebaseui-auth-container', {
   });
 
 var uiConfig = {
-callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-    // User successfully signed in.
-    // Return type determines whether we continue the redirect automatically
-    // or whether we leave that to developer to handle.
-    return true;
+    callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+        },
+        uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+        }
     },
-    uiShown: function() {
-    // The widget is rendered.
-    // Hide the loader.
-    document.getElementById('loader').style.display = 'none';
-    }
-},
-// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-signInFlow: 'popup',
-signInSuccessUrl: 'https://yearbook-hhs.web.app/defualt.html',
-signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
-],
-// Terms of service url.
-tosUrl: 'www.mingyfong.github.io',
-// Privacy policy url.
-privacyPolicyUrl: 'www.mingyfong.github.io'
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'https://yearbook-hhs.web.app/defualt.html',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: 'www.mingyfong.github.io',
+    // Privacy policy url.
+    privacyPolicyUrl: 'www.mingyfong.github.io'
 };
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+    document.getElementById('user-signed-in').style.display = 'block';
+    document.getElementById('user-signed-out').style.display = 'none';
+    document.getElementById('name').textContent = user.displayName;
+    document.getElementById('email').textContent = user.email;
+    document.getElementById('phone').textContent = user.phoneNumber;
+    if (user.photoURL) {
+      var photoURL = user.photoURL;
+      // Append size to the photo URL for Google hosted images to avoid requesting
+      // the image with its original resolution (using more bandwidth than needed)
+      // when it is going to be presented in smaller size.
+      if ((photoURL.indexOf('googleusercontent.com') != -1) ||
+          (photoURL.indexOf('ggpht.com') != -1)) {
+        photoURL = photoURL + '?sz=' +
+            document.getElementById('photo').clientHeight;
+      }
+      document.getElementById('photo').src = photoURL;
+      document.getElementById('photo').style.display = 'block';
+    } else {
+      document.getElementById('photo').style.display = 'none';
+    }
+  };
