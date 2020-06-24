1a83cbdb93ad8b5849e6760a3b969d338b64fdfd
Ming Fong <mingfong07@hotmail.com>
2020-06-23 20:31:31 -0700

    Revert "Store image as String instead of Blob"
    This reverts commit 47652636b5f5aeea04d972538da8c88ae5967f88.

--- public/save.js
+++ public/save.js
@@ -1,49 +1,50 @@
 function upload() {
     const canvas = document.querySelector("#canvas");
-    var dataURL = canvas.toDataURL();
 
     // Create a root reference
     var storageRef = firebase.storage().ref();
     
-    var uploadTask = storageRef.child('test').putString(dataURL);
+    canvas.toBlob(function(blob){
+        var uploadTask = storageRef.child('test').put(blob);
 
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function(snapshot) {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                break;
-        }
-    }, function(error) {
-        console.log("Failed to upload image to the server.");
-        console.log(error);
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-            case 'storage/unauthorized':
-                // User doesn't have permission to access the object
-                break;
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        }, function(error) {
+            console.log("Failed to upload image to the server.");
+            console.log(error);
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
 
-            case 'storage/canceled':
-                // User canceled the upload
-                break;
+                case 'storage/canceled':
+                    // User canceled the upload
+                    break;
 
-            case 'storage/unknown':
-                // Unknown error occurred, inspect error.serverResponse
-                break;
-        }
-    }, function() {
-    // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-            console.log('File available at', downloadURL);
-            window.alert("Upload successful");
+                case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    break;
+            }
+        }, function() {
+        // Upload completed successfully, now we can get the download URL
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log('File available at', downloadURL);
+                window.alert("Upload successful");
+            });
         });
     });
 };
@@ -59,20 +60,13 @@ function getImage() {
     // Get the download URL
     pathReference.getDownloadURL().then(function(url) {
 
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = 'text';
-        xhr.onload = function(event) {
-            var dataString = xhr.response;
-             // Draw the png file onto the canvas
-            drawing = new Image();
-            drawing.src = dataString; // can also be a remote URL e.g. http://
-            drawing.onload = function() {
-                ctx.drawImage(drawing, 0, 0);
-            };
-            console.log("Image from server drawn onto canvas. URL = ", url);
+        // Draw the png file onto the canvas
+        drawing = new Image();
+        drawing.src = url; // can also be a remote URL e.g. http://
+        drawing.onload = function() {
+            ctx.drawImage(drawing,0,0);
         };
-        xhr.open('GET', url);
-        xhr.send();
+        console.log("Image from server drawn onto canvas. URL = ", url);
     }).catch(function(error) {
         console.log("Failed to get image from the server.");
         console.log(error);
