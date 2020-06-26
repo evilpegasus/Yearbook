79521f008297e4fe1d48a10c1d682cd443afe5a5
Ming Fong <mingfong07@hotmail.com>
2020-06-26 02:58:09 -0700

    broken

--- public/save.js
+++ public/save.js
@@ -18,7 +18,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 serveID = urlParams.get('user')
-if (serveID == "" || serveID == this.currentUser.uid) {
+if (serveID == "" || serveID == this.currentUser.uid) { // currentUser is undefined <================= FIX THIS
     serveID = currentUser.uid;
     document.getElementById('owner').innerHTML("You are viewing your own yearbook");
 } else {
