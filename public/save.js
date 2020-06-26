2b6a1ef08ecff0cb3cf4a1931a3085371ec2eaa5
arnavthareja <arnav.thareja25@gmail.com>
2020-06-25 23:56:29 -0700

    Fix promise chaining

--- public/save.js
+++ public/save.js
@@ -8,53 +8,51 @@ function upload() {
         canvas.toBlob(function(blob){
 
             // Create old.png if first time uploading, otherwise write to temp.png
-            storageRef.child(currentUser.uid + "/old.png").getDownloadURL().then(onResolve, onReject);
-            // old.png already exists, write to temp.png
-            function onResolve() {
+            storageRef.child(currentUser.uid + "/old.png").getDownloadURL().then(value => {
+                // old.png already exists, write to temp.png
                 var uploadTask = storageRef.child(currentUser.uid + '/temp.png').put(blob);
-            }
-            // old.png does not exist, create it
-            function onReject(error) {
+            }, reason => {
+                // old.png does not exist, create it
                 var uploadTask = storageRef.child(currentUser.uid + '/old.png').put(blob);
-            }
-
-            // Listen for state changes, errors, and completion of the upload.
-            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-            function(snapshot) {
-                // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case firebase.storage.TaskState.PAUSED: // or 'paused'
-                        console.log('Upload is paused');
-                        break;
-                    case firebase.storage.TaskState.RUNNING: // or 'running'
-                        console.log('Upload is running');
-                        break;
-                }
-            }, function(error) {
-                console.log("Failed to upload image to the server.");
-                console.log(error);
-                // A full list of error codes is available at
-                // https://firebase.google.com/docs/storage/web/handle-errors
-                switch (error.code) {
-                    case 'storage/unauthorized':
-                        // User doesn't have permission to access the object
-                        break;
-    
-                    case 'storage/canceled':
-                        // User canceled the upload
-                        break;
-    
-                    case 'storage/unknown':
-                        // Unknown error occurred, inspect error.serverResponse
-                        break;
-                }
-            }, function() {
-            // Upload completed successfully, now we can get the download URL
-                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                    console.log('File available at', downloadURL);
-                    window.alert("Upload successful");
+            }).then(function() {
+                // Listen for state changes, errors, and completion of the upload.
+                uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                function(snapshot) {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log('Upload is running');
+                            break;
+                    }
+                }, function(error) {
+                    console.log("Failed to upload image to the server.");
+                    console.log(error);
+                    // A full list of error codes is available at
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            break;
+        
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            break;
+        
+                        case 'storage/unknown':
+                            // Unknown error occurred, inspect error.serverResponse
+                            break;
+                    }
+                }, function() {
+                // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                        console.log('File available at', downloadURL);
+                        window.alert("Upload successful");
+                    });
                 });
             });
         });
@@ -62,7 +60,6 @@ function upload() {
         window.alert("Something went wrong while uploading your image:\n" + error.message);
         console.log(error);
     }
-    
 };
 
 function getImage() {
