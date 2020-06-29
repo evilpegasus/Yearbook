59bd2e51e86948b6d4e67f537c3cd28a3b4492db
arnavthareja <arnav.thareja25@gmail.com>
2020-06-28 21:40:37 -0700

    Remove unnecessary functions

--- public/save.js
+++ public/save.js
@@ -241,42 +241,10 @@ function download() {
     // Create a reference with an initial file path and name
     var storageRef = firebase.storage().ref();
     var uploadTask = storageRef.child(serveID + '/' + serveID).putString(serveID).then(function() {
-        // Listen for state changes, errors, and completion of the upload.
-        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-        function(snapshot) {
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case firebase.storage.TaskState.PAUSED: // or 'paused'
-                    console.log('Upload is paused');
-                    break;
-                case firebase.storage.TaskState.RUNNING: // or 'running'
-                    console.log('Upload is running');
-                    break;
-            }
-        }, function(error) {
-            console.log("Failed to upload image to the server.");
-            console.log(error);
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
-            switch (error.code) {
-                case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    break;
-
-                case 'storage/canceled':
-                    // User canceled the upload
-                    break;
-
-                case 'storage/unknown':
-                    // Unknown error occurred, inspect error.serverResponse
-                    break;
-            }
-        }, function() {
         // Upload completed successfully, now we can get the download URL
-            window.alert("A copy of your yearbook will be emailed to you soon!");
-        });
+        window.alert("A copy of your yearbook will be emailed to you soon!");
+    }).catch(function(error) {
+        window.alert('An error occurred, please try again later.');
     });
 }
 
