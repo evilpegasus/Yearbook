cc14aca56eaf3c6717b40af6842e057fa7f6e61f
arnavthareja <arnav.thareja25@gmail.com>
2020-06-28 20:30:38 -0700

    Create cloud function to email users their yearbook

--- functions/index.js
+++ functions/index.js
@@ -21,6 +21,9 @@ const mailTransport = nodemailer.createTransport({
     },
 });
 
+// variables for sendYearbookCopyEmail
+const db = admin.firestore();
+
 // App name to include in the emails
 const APP_NAME = 'Yearbook 2020';
 
@@ -104,40 +107,77 @@ async function sendWelcomeEmail(email, displayName, uid) {
 
     // Body of the email
     mailOptions.subject = 'Welcome to ' + APP_NAME + '!';
-    mailOptions.text = "Hey " + displayName + "! \nWelcome to " + APP_NAME + ". We hope you will enjoy your yearbook. To share your yearbook with your friends, send them this link: \n\n https://yearbook-hhs.web.app/app.html?user=" + uid + "\n\nAnyone with your link can draw on your page. Only send it to people you trust. \n\nIf you have any question or issues, please email us at yearbook2020app@gmail.com. \n\n -The Yearbook Team";
+    mailOptions.text = "Hi " + displayName + "! \nWelcome to " + APP_NAME + ". We hope you will enjoy your yearbook. To share your yearbook with your friends, send them this link: \n\n https://yearbook-hhs.web.app/app.html?user=" + uid + "\n\nAnyone with your link can draw on your page. Only send it to people you trust. \n\nIf you have any questions or concerns, please email us at yearbook2020app@gmail.com. \n\n -The Yearbook Team";
     await mailTransport.sendMail(mailOptions);
     console.log('New welcome email sent to:', email);
     return null;
 }
 
-/*
-// TODO: Send an email with the image when export button pressed
-// File will be uploaded containing user information to email to
+exports.exportYearbook = functions.storage.object().onFinalize(async (object) => {    
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
 
-exports.exportYearbook = functions.storage.object().onFinalize(async (object) => {
-    const email = user.email; // The email of the user
-    const displayName = user.displayName; // The display name of the user
-    const uid = user.uid; // The unique uid of the user
+    // Exit if file is an image
+    if (contentType.startsWith('image/')) {
+        return console.log('This is an image.');
+    }
 
-    return sendWelcomeEmail(email, displayName, uid);
+    // const uid = path.basename(filePath, path.extname(filePath));
+    const uid = path.parse(filePath).name;
+    const fileDir = path.dirname(filePath);
+    const oldFile = fileDir + "/old.png";
+    const oldName = path.basename(oldFile);
+
+    // Download file from bucket
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), oldName);
+    const tempPdfPath = path.join(os.tmpdir(), "old.pdf");
+    await bucket.file(filePath).download({destination: tempFilePath});
+    console.log('Image downloaded locally to', tempFilePath);
+
+    // Merge the images using ImageMagick
+    await spawn('convert', [tempFilePath, tempPdfPath]);
+    console.log('Image converted to PDF at', tempPdfPath);
+
+    // delete the request file
+    bucket.file(filePath).delete().then(function() {
+        // Deleted successfully
+        console.log('Request file deleted');
+    }).catch(function(error) {
+        // An error occurred
+        console.log(error);
+    });
+
+    db.collection("users").doc(uid).get().then(function(doc) {
+        const displayName = doc.get('displayName');
+        const email = doc.get('email');
+        return sendYearbookCopyEmail(email, displayName, tempFilePath, tempPdfPath);
+    });
 });
 
-// Sends a welcome email to the given user.
-async function sendWelcomeEmail(email, displayName, uid) {
+// Sends a copy of the yearbook to the given user.
+async function sendYearbookCopyEmail(email, displayName, pngPath, pdfPath) {
      const mailOptions = {
         from: APP_NAME + " <noreply@firebase.com>",
         to: email,
         attachments: [{
             filename: 'yearbook.png',
-            content: fs.createReadStream('/complete-path/image.jpg')
+            content: fs.createReadStream(pngPath)
+        }, {
+            filename: 'yearbook.pdf',
+            content: fs.createReadStream(pdfPath)
         }]
     };
 
     // Body of the email
-    mailOptions.subject = 'Welcome to ' + APP_NAME + '!';
-    mailOptions.text = "Hey " + displayName + "\n! Welcome to " + APP_NAME + ". We hope you will enjoy your yearbook. To share your yearbook with your friends, send them this link: \n\n https://yearbook-hhs.web.app/app.html?user=" + uid + "\n\nIf you have any question or issues, please email us at yearbook2020app@gmail.com. \n\n -The Yearbook Team";
+    mailOptions.subject = 'Your yearbook from ' + APP_NAME;
+    mailOptions.text = "Hi " + displayName + "\n! Attached is your yearbook from " + APP_NAME + ". We hope you will enjoy it. If you have any questions or concerns, please email us at yearbook2020app@gmail.com. \n\n -The Yearbook Team";
     await mailTransport.sendMail(mailOptions);
-    console.log('New welcome email sent to:', email);
+    console.log('Yearbook sent to: ', email);
+    
+    // Delete temporary files once email is sent
+    fs.unlinkSync(pngPath);
+    fs.unlinkSync(pdfPath);
     return null;
 }
-*/
--- public/save.js
+++ public/save.js
@@ -25,8 +25,7 @@ firebase.auth().onAuthStateChanged(function(user) {
                 email: currentUser.email,
                 theme: themeSelector.options[themeSelector.selectedIndex].value,
                 userID: currentUser.uid,
-                pfpURL: "",
-                numDownloadEmails: 0
+                pfpURL: ""
             }).then(function() {
                 console.log("Document successfully written");
             }).catch(function(error) {
@@ -223,6 +222,7 @@ function getImage(alert = true) {
 }
 
 function download() {
+    /*
     var storage = firebase.storage();
     var pathReference = storage.ref(currentUser.uid + '/old.png');
 
@@ -235,6 +235,48 @@ function download() {
         link.click();
         document.body.removeChild(link);
         delete link;
+    });
+    */
+    
+    // Create a reference with an initial file path and name
+    var storageRef = firebase.storage().ref();
+    var uploadTask = storageRef.child(serveID + '/' + serveID).putString(serveID).then(function() {
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                    console.log('Upload is running');
+                    break;
+            }
+        }, function(error) {
+            console.log("Failed to upload image to the server.");
+            console.log(error);
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+                case 'storage/unauthorized':
+                    // User doesn't have permission to access the object
+                    break;
+
+                case 'storage/canceled':
+                    // User canceled the upload
+                    break;
+
+                case 'storage/unknown':
+                    // Unknown error occurred, inspect error.serverResponse
+                    break;
+            }
+        }, function() {
+        // Upload completed successfully, now we can get the download URL
+            window.alert("A copy of your yearbook will be emailed to you soon!");
+        });
     });
 }
 
